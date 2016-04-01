c6263989affc3c74f98b0809d8a6262a4d0c924b
butlerx <cianbutlerx@gmail.com>
2016-04-01 15:41:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+var TinyURL = require('tinyurl');
+var os = require('os');
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+controller.hears(['google (.*)', 'lmg (.*)'], 'ambient', function(bot, message) {
+    var url = '';
+    var args = message.match[0].split(' ');
+    if (args[0] === 'google') {
+      url = google(message.match[1]);
+      bot.reply(message, url)
+    } else {
+      TinyURL.shorten(letme(message.match[1]), function(res) {
+        url = res;
+        bot.reply(message, url)
+      });
+    }
+});
+
+function urlencode(str) {
+  str = escape(str);
+  str = str.replace('+', '%2B');
+  str = str.replace('%20', '+');
+  str = str.replace('*', '%2A');
+  str = str.replace('/', '%2F');
+  str = str.replace('@', '%40');
+  return str;
+};
+
+function letme(str) {
+  str = urlencode(str);
+  var url = 'http://letmegooglethatforyou.com/?q=' + str;
+  return url;
+}
+
+function google(str) {
+  str = urlencode(str);
+  var url = 'https://google.ie/?q=' + str;
+  return url;
+}
